70587f883a48bfee3f93b75e2ad09c29d71edd16
Costa-atsoC <gonca.costa30@gmail.com>
2024-05-08 10:29:17 +0100

    Flag and stuff

--- main.go
+++ main.go
@@ -5,15 +5,15 @@ import(
 	"math/rand/v2"
 	// "os"
 	"flag"
-	// "time"
+	"time"
 )
 
 var line = 1
 var column = 1
 var steps = 1
+var sleep = 1
 
 const (
-	sleep  = 100
 	alive  = "🟩"
 	dead   = "🟥"
 ) 	
@@ -33,7 +33,7 @@ func GenerateWorld() World{
 func Seed(w World){
 	for _,row := range w {
 		for col := range row{
-			if rand.IntN(4) == 1{
+			if rand.IntN(3) == 1{ //In here we change the probability of the cubes being alive
 				row[col] = true
 			}
 		}
@@ -81,33 +81,56 @@ func Rules(w World, x, y int) bool{
 	return false
 }
 
+func CheckBoard(w World) bool{
+	for _,row := range w {
+		for col := range row{
+			if row[col]{
+				return true
+			}
+		}
+	}
+	return false
+}
+
 func main(){
 	// Flags
-	widthFlag := flag.Int("w", 5, "a int")
-	heighFlag := flag.Int("h", 5, "an int")
-	stepFlag := flag.Int("s", 1, "an int")
+	widthFlag := flag.Int("x", 5, "The width of the board.")
+	heighFlag := flag.Int("y", 5, "The width of the board.")
+	stepFlag := flag.Int("s", 1, "The number of iterations the program will go through.")
+	timeFlag := flag.Int("t", 1000, "The time the program will sleep between the boards. This time is in milliseconds.")
 
 	flag.Parse()
 
 	line = *widthFlag
 	column = *heighFlag
 	steps = *stepFlag
+	sleep = *timeFlag
+
+	//To know how many steps it took until the world dies
+	numberSteps := 0
 
 	world := GenerateWorld()
 	Seed(world)
 	PrintWorld(world)
+	time.Sleep(time.Duration(sleep) * time.Millisecond)
 
 	for i := 0; i< steps; i++ {
 		newWorld := GenerateWorld()
 		for indexRow, row := range world {
 			for indexCol, _ := range row {
-				// fmt.Println(indexRow, indexCol)
 				newWorld[indexRow][indexCol] = Rules(world, indexRow, indexCol)
 			}
 		}
-		
 		fmt.Println()
 		PrintWorld(newWorld)
 		world = newWorld
+		numberSteps++
+
+		if!CheckBoard(newWorld) {
+			fmt.Println("It took ", numberSteps, " until the world died.")
+			break 
+		}
+		time.Sleep(time.Duration(sleep) * time.Millisecond)
+
 	}
 }
--- makefile
+++ makefile
@@ -2,7 +2,7 @@ BINARY_NAME=main
 
 run: 
 	GOARCH=amd64 GOOS=linux go build -o ${BINARY_NAME} main.go
-	./${BINARY_NAME}
+	./${BINARY_NAME} -s=100 -t=150 -x=20 -y=15
 
 build:
 	GOARCH=amd64 GOOS=linux go build -o ${BINARY_NAME} main.go
